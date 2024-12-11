6907fb3c8e52023f4385a8688c67256a54c845b0
Salyakin <Salyakin666@gmail.com>
2024-12-12 03:46:58 +0500

    Add files via upload

---
+++ Module_2_3.py
@@ -0,0 +1,18 @@
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+i = 0
+
+while i < len(my_list):
+    num = my_list[i]
+    i += 1
+    if num == 0:
+        continue
+    elif num < 0:
+        break
+    elif i == len(my_list):
+        print(num)
+    else:
+        print(num)
+
+
+
+
